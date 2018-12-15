69294d74ac9c8e44ff299169e01fdc66d7bffe80
20163156 <97jjy@naver.com>
2018-12-16 02:21:06 +0900

    칼로리 데이터 추가

--- AndroidStudioProjects/dietapp/app/src/main/java/com/kmu/dietapp/DBHelper2.java
+++ AndroidStudioProjects/dietapp/app/src/main/java/com/kmu/dietapp/DBHelper2.java
@@ -24,6 +24,31 @@ public class DBHelper2 extends SQLiteOpenHelper {
 
         db.execSQL("INSERT INTO calories VALUES(NULL,'rice','310');");
         db.execSQL("INSERT INTO calories VALUES(NULL,'americano','4');");
+        db.execSQL("INSERT INTO calories VALUES(NULL,'kimchi','25');");
+        db.execSQL("INSERT INTO calories VALUES(NULL,'dufu','88');");
+        db.execSQL("INSERT INTO calories VALUES(NULL,'apple','130');");
+        db.execSQL("INSERT INTO calories VALUES(NULL,'egg','80');");
+        db.execSQL("INSERT INTO calories VALUES(NULL,'sweetpotato','128');");
+        db.execSQL("INSERT INTO calories VALUES(NULL,'banana','93');");
+        db.execSQL("INSERT INTO calories VALUES(NULL,'strawberry','36');");
+        db.execSQL("INSERT INTO calories VALUES(NULL,'almond','7');");
+        db.execSQL("INSERT INTO calories VALUES(NULL,'beer','74');");
+        db.execSQL("INSERT INTO calories VALUES(NULL,'coke','108');");
+        db.execSQL("INSERT INTO calories VALUES(NULL,'pork and kimchi stew','128');");
+        db.execSQL("INSERT INTO calories VALUES(NULL,'sea mustard soup','83');");
+        db.execSQL("INSERT INTO calories VALUES(NULL,'sweet and sour pork','228');");
+        db.execSQL("INSERT INTO calories VALUES(NULL,'kimbap','318');");
+        db.execSQL("INSERT INTO calories VALUES(NULL,'cucumber','19');");
+        db.execSQL("INSERT INTO calories VALUES(NULL,'bread','111');");
+        db.execSQL("INSERT INTO calories VALUES(NULL,'noodles with black soybean sauce','785');");
+        db.execSQL("INSERT INTO calories VALUES(NULL,'hamburger','270');");
+        db.execSQL("INSERT INTO calories VALUES(NULL,'pizza','404');");
+        db.execSQL("INSERT INTO calories VALUES(NULL,'chiken','249');");
+        db.execSQL("INSERT INTO calories VALUES(NULL,'tteokbokki','280');");
+        db.execSQL("INSERT INTO calories VALUES(NULL,'jokbal','394');");
+        db.execSQL("INSERT INTO calories VALUES(NULL,'grilled fish','379');");
+
+
 
     }
     @Override
