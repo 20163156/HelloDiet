86f7af11e8f3ae825515442de2736e8b8ca0c3d3
20163156 <97jjy@naver.com>
2018-12-16 23:58:10 +0900

    노트 저장을 위한shared preferences 추가

--- AndroidStudioProjects/dietapp/app/src/main/java/com/kmu/dietapp/Note.java
+++ AndroidStudioProjects/dietapp/app/src/main/java/com/kmu/dietapp/Note.java
@@ -1,13 +1,38 @@
 package com.kmu.dietapp;
 
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class Note extends AppCompatActivity {
+    public static final String KEY_MY_PREFERENCE = "my_preference";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_note);
+
+
+        SharedPreferences prefs_note = getSharedPreferences("pref_note", MODE_PRIVATE);
+        String text = prefs_note.getString(KEY_MY_PREFERENCE, "");
+        final EditText edit = (EditText) findViewById(R.id.note);
+        edit.setText(text);
+
+    }
+
+    protected void onStop(){
+        super.onStop();
+
+        EditText editText = (EditText) findViewById(R.id.note);
+        String text = editText.getText().toString();
+
+        // 데이타를저장합니다.
+        SharedPreferences prefs_note = getSharedPreferences("pref_note", MODE_PRIVATE);
+        SharedPreferences.Editor editor = prefs_note.edit();
+        editor.putString(KEY_MY_PREFERENCE, text);
+
+        editor.commit();
+
     }
 }
